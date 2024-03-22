92807a23711200087e49e0b74ab18ef388aedb5b
Daniel Rocha <102672001+Dannick10@users.noreply.github.com>
2024-03-22 15:53:45 -0300

    ::feat buttons pages

--- src/Pages/Livros.jsx
+++ src/Pages/Livros.jsx
@@ -12,7 +12,7 @@ const Livros = () => {
   const [checked, setChecked] = useState(false);
  
   const {bibleJson,bookName,SetBookName,bookChapter,SetBookChapter,loading,chapterSize,SetChapterSize} = useFetchBible()
-  
+    
   return (
     <main className={styles.section_books}>
          
@@ -32,7 +32,19 @@ const Livros = () => {
        <section className={styles.book}>
        <aside className={styles.bookRead}>
          {loading && <div className={styles.loadingBooks}></div>}
+
          {bibleJson && <Books bookTitle={bibleJson.reference} bookChapter={bookName} bookText={bibleJson.verses}/>}
+
+         <div className={styles.controls}>
+
+              <div className={styles.left} style={bookChapter==1 ?{opacity:'0.3', pointerEvents:'none'}:{opacity:'1'}}><i className="fa-solid fa-chevron-left" onClick={()=>{SetBookChapter(bookChapter - 1),scrollTo(0,0)}}></i></div>
+         
+
+            <div className={styles.right} style={chapterSize == bookChapter?{opacity:'0.3',pointerEvents:'none'}:{opacity:'1'}}><i className="fa-solid fa-chevron-right" onClick={()=>{SetBookChapter(bookChapter + 1), scrollTo(0,0)}}></i></div>
+
+         </div>
+         
+
        </aside>
           <aside className={styles.bookNumber}>
           {!loading &&(
