c6afecdddf439edfff80d1a0623775994109ba49
Daniel Rocha <102672001+Dannick10@users.noreply.github.com>
2024-03-30 19:36:18 -0300

    verficiação

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -4,23 +4,50 @@ import { useState } from 'react'
 
 const Register = () => {
 
+  const [displayName, SetDisplayName] = useState('')
+  const [email,SetEmail] = useState('')
+  const [password,SetPassword] = useState('')
+  const [confirmpassword,SetConfirmPassword] = useState('')
+  const [error,SetError] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    SetError('')
+
+    const user = { 
+      displayName,
+      email,
+      password,
+      confirmpassword
+    }
+
+    if(password !== confirmpassword){
+      SetError('as Senhas precisam ser iguais')
+      return
+    }
+
+    console.log(user)
+
+  }
+
   const [viewpassword,SetViewPassword] = useState('password')
   return (
     <div className='register'>
           <h1>Crie conta</h1>
          <section className='register_main'>  
-           <form className='form'>
+           <form className='form' onSubmit={handleSubmit}>
              <label>
                <span>Nome</span>
-                 <input type="text" placeholder='nome' />
+                 <input type="text" placeholder='nome' required onClick={(e)=>SetDisplayName(e.target.value)}/>
              </label>
              <label>
                <span>Email</span>
-               <input type="email" placeholder='endereço de email' />
+               <input type="email" placeholder='endereço de email' required onClick={(e)=>SetEmail(e.target.value)}/>
              </label>
              <label>
                <span>Senha</span>
-               <input type={viewpassword == 'password'?'password':'text'} placeholder='senha' required  />
+               <input type={viewpassword == 'password'?'password':'text'} placeholder='senha' required  onClick={(e)=>SetPassword(e.target.value)} />
                <div className='visible'>
                 {viewpassword == 'password'?(
                   <i className="fa-regular fa-eye-slash" onClick={()=>SetViewPassword('text')}></i>
@@ -31,7 +58,7 @@ const Register = () => {
              </label>
              <label>
                <span>Confirmar senha</span>
-               <input type={viewpassword == 'password'?'password':'text'} placeholder='confirme a senha' required  />
+               <input type={viewpassword == 'password'?'password':'text'} placeholder='confirme a senha' required  onClick={(e)=>SetConfirmPassword(e.target.value)} />
                <div className='visible'>
                 {viewpassword == 'password'?(
                   <i className="fa-regular fa-eye-slash" onClick={()=>SetViewPassword('text')}></i>
@@ -43,6 +70,11 @@ const Register = () => {
              <label>
               <input className='btn' type="submit" value="Cadastrar" />
              </label>
+             {error && (
+              <div className='error'>
+                {error}
+              </div>
+             )}
            </form>
            <div className='form showcase'>
              <div className='showcase_text'>
