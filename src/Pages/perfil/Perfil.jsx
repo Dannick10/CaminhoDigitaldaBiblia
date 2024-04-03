a421a3ea69ab43652fce32eee3b022c5864fc1b2
Daniel Rocha <102672001+Dannick10@users.noreply.github.com>
2024-04-02 21:26:08 -0300

    dashboard design

--- src/Pages/perfil/Perfil.jsx
+++ src/Pages/perfil/Perfil.jsx
@@ -1,8 +1,40 @@
 import React from 'react'
+import styles from './perfil.module.css'
 
 const Perfil = () => {
+
+
   return (
-    <div>Perfil</div>
+    <div className={styles.perfil}>
+
+          <div className={styles.perfil_info}>
+          <i className="fa-solid fa-user"></i>
+            <h2>DASHBOARD</h2>
+            <p>Caminho Digital da Bíblia</p>
+            <button className='btn btnP'>SAIR</button>
+          </div>
+
+        <sectopm className={styles.perfil_dashboard}>
+
+          <ul>
+            <li>
+            <i className="fa-solid fa-heart"></i>
+              <p>Textos Favoritos</p>
+              </li>
+
+            <li>
+            <i className="fa-solid fa-magnifying-glass"></i>
+              <p>Históricos</p>
+              </li>
+
+              <li>
+              <i className="fa-solid fa-table-columns"></i>
+              <p>Postagens</p>
+              </li>
+
+          </ul>
+        </sectopm>
+    </div>
   )
 }
 
