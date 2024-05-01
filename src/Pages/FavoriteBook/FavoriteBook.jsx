761cb1cb1f37728d9a1eecec498b3a5f9d3bf0dd
Daniel Rocha <Danmaggot2@gmail.com>
2024-05-01 18:26:42 -0300

    favorite book

--- src/Pages/FavoriteBook/FavoriteBook.jsx
+++ src/Pages/FavoriteBook/FavoriteBook.jsx
@@ -5,13 +5,24 @@ import { useFetchDocuments } from '../../hooks/useFetchDocuments'
 
 const FavoriteBook = () => {
 
-    const { documents: books, response } = useFetchDocuments('book')
-    console.log(document)
+    const { documents: books, loading } = useFetchDocuments('book')
+    console.log(books)
 
 
   return (
     <div className={style.favorite_section}>
+        {loading && <p className='loadingBooks'></p>}
         FavoriteBook
+
+        {books && books.map((book) => (
+            <>
+                <div className={style.book_favorite}>
+                    <p>{book.text}</p>
+                    <p>{book.nameBook}</p>
+                    <p>{book.id_book.replace('-',' ')}</p>
+                </div>
+            </>
+        ))}
         </div>
   )
 }
